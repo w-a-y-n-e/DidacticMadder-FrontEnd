500c376c80f155cc3fb19ed235517eaddad241a1
Wayne Simonsen <wayne.a.simonsen@gmail.com>
2021-02-25 05:04:49 +0000

    Initial files

---
+++ guacamole-ctfd-authentication/src/main/java/org/apache/guacamole/auth/CTFdAuthenticationProvider.java
@@ -0,0 +1,74 @@
+package org.apache.guacamole.auth;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import org.apache.guacamole.GuacamoleException;
+import org.apache.guacamole.net.auth.AbstractAuthenticationProvider;
+import org.apache.guacamole.net.auth.Credentials;
+import org.apache.guacamole.net.auth.credentials.CredentialsInfo;
+import org.apache.guacamole.net.auth.UserContext;
+import org.apache.guacamole.net.auth.AuthenticatedUser;
+import org.apache.guacamole.net.auth.credentials.GuacamoleInvalidCredentialsException;
+import org.apache.http.impl.client.BasicCookieStore;
+import org.apache.http.impl.cookie.BasicClientCookie;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.HttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.HttpResponse;
+import java.io.IOException;
+
+
+/**
+ * Authentication provider to use cookie from CTFd through custom CTFd plugin.
+ * Used in conjunction with database authentication which stores connections.
+ */
+public class CTFdAuthenticationProvider extends AbstractAuthenticationProvider {
+
+    @Override
+    public String getIdentifier() {
+        return "ctfd-authentication";
+    }
+
+    public AuthenticatedUser authenticateUser(Credentials credentials)
+            throws GuacamoleException {
+        // Pull HTTP header from request if present
+        HttpServletRequest request = credentials.getRequest();
+	if (request != null) {
+	   Cookie[] cookies = request.getCookies();
+	   String cookieName = "session";
+           String cookieValue = "";
+           String externalUsername = "";
+           if (cookies != null) {
+           for ( int i=0; i<cookies.length; i++) {
+              Cookie cookie = cookies[i];
+              if (cookieName.equals(cookie.getName()))
+                 cookieValue = cookie.getValue();
+            }
+           }
+
+                CTFdAuthenticatedUser authenticatedUser = new CTFdAuthenticatedUser();
+                BasicCookieStore cookieStore = new BasicCookieStore();
+                BasicClientCookie clientCookie = new BasicClientCookie("session", cookieValue);
+                clientCookie.setDomain("127.0.0.1");
+                clientCookie.setPath("/");
+                cookieStore.addCookie(clientCookie);
+                HttpClient client = HttpClientBuilder.create().setDefaultCookieStore(cookieStore).build();
+                try{
+                        final HttpGet httpRequest = new HttpGet("http://127.0.0.1/getusername");
+                        HttpResponse httpResponse = client.execute(httpRequest);
+                        externalUsername=EntityUtils.toString(httpResponse.getEntity());
+                    //assertThat(response.getStatusLine().getStatusCode(), equalTo(200));
+                }
+                catch (IOException e) {
+                        e.printStackTrace();
+                }
+                authenticatedUser.init(externalUsername, credentials);
+                return authenticatedUser;
+        }
+
+        // Authentication not provided yet.
+        throw new GuacamoleInvalidCredentialsException("Invalid login.", CredentialsInfo.USERNAME_PASSWORD);
+
+    }
+}
